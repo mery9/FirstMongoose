8343f087613c8c2044ae297b2aaddaea2eb68d7e
Kantinan <kaseam008@gmail.com>
2022-12-21 13:10:03 +0700

    FirstFinishMongoose

---
+++ models/customer.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+const CustomerSchema = mongoose.Schema(
+    {
+        CustomerId: Number,
+        Fullname:   String,
+        Address:    String
+    },
+    {
+        versionKey: false
+    }
+)
+
+
+module.exports = mongoose.model('Customer', CustomerSchema);
